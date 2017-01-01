166d48abc05cf60080c06b572134b12cdab12e35
Craig Doremus <cdoremus@gmail.com>
2017-01-01 16:30:11 -0500

    Added @CrossOrigin to work with separate frontend

--- src/main/java/org/cdoremus/myshowcase/controller/DisplayItemRestController.java
+++ src/main/java/org/cdoremus/myshowcase/controller/DisplayItemRestController.java
@@ -3,7 +3,6 @@ package org.cdoremus.myshowcase.controller;
 import org.cdoremus.myshowcase.model.DisplayItem;
 import org.cdoremus.myshowcase.service.DisplayItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +14,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping(value = "/displayItem")
+@CrossOrigin(origins = { "*" })
 public class DisplayItemRestController {
 
     @Autowired
--- src/main/java/org/cdoremus/myshowcase/controller/UserRestController.java
+++ src/main/java/org/cdoremus/myshowcase/controller/UserRestController.java
@@ -5,12 +5,7 @@ import org.cdoremus.myshowcase.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *
@@ -18,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(value = "/user")
+@CrossOrigin(origins = { "*" })
 public class UserRestController {
 
     @Autowired
