766d87c8207364dc7a9f28bfa02365c596411227
Craig Doremus <cdoremus@gmail.com>
2017-01-13 15:58:10 -0500

    Removed unused imports

--- src/test/java/org/cdoremus/myshowcase/controller/HomeRestControllerTest.java
+++ src/test/java/org/cdoremus/myshowcase/controller/HomeRestControllerTest.java
@@ -1,13 +1,11 @@
 package org.cdoremus.myshowcase.controller;
 
-import static org.hamcrest.core.StringContains.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import org.cdoremus.myshowcase.model.CsrfToken;
-import org.hamcrest.core.StringContains;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,13 +16,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.ContentResultMatchers;
-
-import java.awt.*;
 
 
 /**
- * Created by craig on 12/18/16.
+ * Unit tests for HomeRestController.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
